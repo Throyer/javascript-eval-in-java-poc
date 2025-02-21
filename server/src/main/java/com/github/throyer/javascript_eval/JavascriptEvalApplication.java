e3da50e2d93ca4b44cbf9929773e93441bd6d70e
Renato <renato.henrique_98@hotmail.com>
2025-02-21 22:04:41 +0000

    inital commit

---
+++ server/src/main/java/com/github/throyer/javascript_eval/JavascriptEvalApplication.java
@@ -0,0 +1,12 @@
+package com.github.throyer.javascript_eval;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptEvalApplication {
+  public static void main(String[] args) {
+    run(JavascriptEvalApplication.class, args);
+  }
+}
---
+++ server/src/test/java/com/github/throyer/javascript_eval/JavascriptEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.throyer.javascript_eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascriptEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
